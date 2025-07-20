89df2573f1d29515cb5045ca40eacdaa26f8dc30
hadeel-halliq <hadeelhalliq@gmail.com>
2025-07-19 22:25:49 -0700

    add navBar

--- src/Components/MainLayout/NavBar.jsx
+++ src/Components/MainLayout/NavBar.jsx
@@ -1,7 +1,36 @@
+import { NavLink } from "react-router-dom";
+
+import { BsTelephone } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
+import { AiOutlineHome } from "react-icons/ai";
+
 export default function NavBar() {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <NavLink to="/">
+        <div>
+            <AiOutlineHome/>
+            <p>الصفحة الرئيسية</p>
+        </div>
+      </NavLink>
+      <NavLink to="/site">
+        <div>
+            <CiLocationOn/>
+            <p>موقع الشركة</p>
+        </div>
+      </NavLink>
+      <NavLink to="/products">
+        <div>
+            <AiOutlineHome/>
+            <p>منتجاتنا</p>
+        </div>
+      </NavLink>
+      <NavLink to="/contact">
+        <div>
+            <BsTelephone/>
+            <p>تواصل معنا</p>
+        </div>
+      </NavLink>
+    </nav>
   )
 }
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,9 +1,10 @@
+import NavBar from "../../Components/MainLayout/NavBar";
 
 
 export default function Home() {
   return (
     <div>
-      Home
+      <NavBar/>
     </div>
   )
 }
