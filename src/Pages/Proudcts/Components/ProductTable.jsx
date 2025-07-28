b41abea6635eb03d577ba0683f1d4bf18e73c1b7
hadeel-halliq <hadeelhalliq@gmail.com>
2025-07-27 22:00:47 -0700

    add shadow to prodct table

--- src/Pages/Proudcts/Components/ProductTable.jsx
+++ src/Pages/Proudcts/Components/ProductTable.jsx
@@ -1,6 +1,6 @@
 export default function ProductTable({ title, headers, data }) {
   return (
-    <div className="rounded-xl  overflow-hidden w-fit border border-gray-400">
+    <div className="rounded-xl  overflow-hidden w-fit border border-gray-400 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] transition duration-500 hover:-translate-y-3 hover:shadow-[0_4px_4px_0_rgba(0,0,0,0.6)] cursor-pointer" >
         <table className="table-auto border-collapse w-[300px] text-center" dir="rtl">
         <thead>
             <tr>
