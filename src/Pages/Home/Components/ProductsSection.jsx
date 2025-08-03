b4eeaf7a07c7a508aecb0a521e7463ed395d0b24
hadeel-halliq <hadeelhalliq@gmail.com>
2025-08-02 21:00:12 -0700

    modification on products section im main page

--- src/Pages/Home/Components/ProductsSection.jsx
+++ src/Pages/Home/Components/ProductsSection.jsx
@@ -12,8 +12,8 @@ export default function ProductsSection() {
     <div className="bg-backGroundColor pt-12 pb-20">
       <div className="container mx-auto px-10">
         <Tittle text="صور من المنتجات"/>
-        <div className="grid place-items-center sm:grid-cols-2 sm:gap-5 md:gap-3 lg:gap-0 md:grid-cols-[repeat(auto-fit,minmax(290px,1fr))] ">
-            {productsImages.map((product, index) => <img src={product} key={index} className="sm:w-[200px] sm:h-[200px] md:w-[280px] md:h-[280px]"/>)}
+        <div className="grid place-items-center sm:grid-cols-2 gap-5 md:gap-3 lg:gap-0 md:grid-cols-[repeat(auto-fit,minmax(290px,1fr))] ">
+            {productsImages.map((product, index) => <img src={product} key={index} className="w-[250px] h-[250px] sm:w-[200px] sm:h-[200px] md:w-[280px] md:h-[280px]"/>)}
         </div>
       </div>
     </div>
